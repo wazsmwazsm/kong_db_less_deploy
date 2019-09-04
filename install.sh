ba96ef50adf05ca8b7ee56ea2c13fa9a395a8f07
qinjiaqi <qinjiaqi@mafengwo.com>
2019-09-04 09:33:36 +0800

    update kong to 1.3.0

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ fi
 
 echo "=====> kong installing..."
 if [ $sys = "ubuntu" ]; then
-    if [ ! -f "./kong-1.2.2.bionic.all.deb" ];then
-        wget https://bintray.com/kong/kong-deb/download_file?file_path=kong-1.2.2.bionic.all.deb \
-            -O kong-1.2.2.bionic.all.deb    
+    if [ ! -f "./kong-1.3.0.bionic.amd64.deb" ];then
+        wget https://bintray.com/kong/kong-deb/download_file?file_path=kong-1.3.0.bionic.amd64.deb \
+            -O kong-1.3.0.bionic.amd64.deb    
     fi
-    sudo dpkg -i kong-1.2.2.bionic.all.deb
+    sudo dpkg -i kong-1.3.0.bionic.amd64.deb
 elif [ $sys = "centos" ]; then
-    if [ ! -f "./kong-1.2.2.bionic.all.deb" ];then
-        wget https://bintray.com/kong/kong-rpm/download_file?file_path=centos/7/kong-1.2.2.el7.noarch.rpm \
-            -O kong-1.2.2.el7.noarch.rpm
+    if [ ! -f "./kong-1.3.0.el7.amd64.rpm" ];then
+        wget https://bintray.com/kong/kong-rpm/download_file?file_path=centos/7/kong-1.3.0.el7.amd64.rpm \
+            -O kong-1.3.0.el7.amd64.rpm
     fi
-    sudo rpm -i kong-1.2.2.el7.noarch.rpm
+    sudo rpm -i kong-1.3.0.el7.amd64.rpm
 fi
 echo "=====> kong installed"
 
